442864668d51b9ffe0362f3ed8bf3620f32fc1a1
Owner PC <PC-Owner-t@outlook.jp>
2023-10-16 18:34:01 +0900

    プロジェクト ファイルを追加します。

---
+++ 2023_10_10_Task_2/2023_10_10_Task_2.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+using namespace std;
+
+void InputArray(int *array, int size) {
+    if (*array == NULL || size == 0) {
+        return;
+    }
+    for (int i = 0; i < size; i++) {
+        printf("%d人目を入力してください。　", i + 1);
+        cin >> array[i];
+    }
+}
+
+double AveArray(int *array,int size) {
+    int sum = 0;
+    for (int i = 0; i < size; i++) {
+        sum += array[i];
+    }
+    return sum / size;
+}
+
+int main()
+{
+    int* array;
+    int size;
+    printf("何名入力しますか？\n>");
+    cin >> size;
+
+    array = new int[size];
+
+    InputArray(&array[0], size);
+
+    cout << "平均は" << AveArray(&array[0], size) << "です。" << endl;
+
+
+    if (array != NULL) {
+        delete[] array;
+
+        array = NULL;
+    }
+}
